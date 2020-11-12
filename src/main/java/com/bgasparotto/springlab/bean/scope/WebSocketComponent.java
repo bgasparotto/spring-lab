ae3469bc86f3b8004f68475d14cd3c6418955f32
bgasparotto <bruno.m.gasparotto@gmail.com>
2020-11-12 18:53:33 +0100

    Removed unused code from the web socket component

--- src/main/java/com/bgasparotto/springlab/bean/scope/WebSocketComponent.java
+++ src/main/java/com/bgasparotto/springlab/bean/scope/WebSocketComponent.java
@@ -4,14 +4,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
-import java.time.Instant;
-
 @Component
 @Scope(scopeName = "websocket", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class WebSocketComponent {
-    private final Instant creationInstant = Instant.now();
-
-    public Instant getCreationInstant() {
-        return creationInstant;
-    }
 }
